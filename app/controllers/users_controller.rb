787765c7cc8372c732eb5fde5884fa97364b6b6f
Ls3109 <louissenac09@gmail.com>
2022-04-21 12:01:09 +0200

    🐛 fix profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
         @userid = params[:user_id]
         @userinfo = User.find(params[:user_id])
         @usercount = @userinfo.tweeets
-        @tweeets = tweeet.all
+        @tweeets=Tweeet.all
     end
 end
